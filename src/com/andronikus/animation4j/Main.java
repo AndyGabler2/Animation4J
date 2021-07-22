8dd74f710b0dfe762bb548700a43118ab0c589b8
AndronikusGameTech <andronikus1335@gmail.com>
2021-07-21 20:49:07 -0400

    Base project up and going. Should be ready to add library classes to.

---
+++ src/com/andronikus/animation4j/Main.java
@@ -0,0 +1,18 @@
+package com.andronikus.animation4j;
+
+/**
+ * Obligatory main class to make Gradle happy.
+ *
+ * @author Andronikus
+ */
+public class Main {
+
+    /**
+     * Obligatory main method to make Gradle happy.
+     *
+     * @param args Do you really think these are used?
+     */
+    public static void main(String[] args) {
+        System.out.println("Stub runner. Use library classes around runner.");
+    }
+}
